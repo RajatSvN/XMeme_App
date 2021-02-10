715e42b07689f2aaeb3bbbf76cbf6b930fd766a8
RajatSvN <rajat901cx@gmail.com>
2021-02-10 20:40:18 +0530

    Reconfiguring Install Script

--- install.sh
+++ install.sh
@@ -2,22 +2,21 @@
 
 # Any installation related commands
 
-sudo su
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 
-echo 'wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -'
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
-echo 'echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list'
+sudo apt update
 
-apt update
+sudo apt install -y mongodb-org
 
-apt install -y mongodb-org
+sudo systemctl start mongod
 
-systemctl start mongod
+sudo systemctl status mongod
 
-systemctl enable mongod
+sudo systemctl enable mongod
 
-
-echo 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash'
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
 . ~/.nvm/nvm.sh
 
@@ -27,9 +26,8 @@ nvm install 13.14.0
 
 nvm use 13.14.0
 
-echo '{ echo -n "IP_ADDRESS="; curl "http://169.254.169.254/latest/meta-data/public-ipv4"; } >> .env'
-
 # Any configuration related commands
+{ echo -n "IP_ADDRESS="; curl "http://169.254.169.254/latest/meta-data/public-ipv4"; } >> .env
 
 npm install 
 
