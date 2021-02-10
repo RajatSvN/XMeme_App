e3646f778c5e58f84959e185228ec6e93a1ec112
RajatSvN <rajat901cx@gmail.com>
2021-02-10 22:03:25 +0530

    documented installation scripts

--- server_run.sh
+++ server_run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# activate nvm
 . ~/.nvm/nvm.sh
 
+# environment variable setup, store public ip of ec2 instance to be used in application
 { echo -n "IP_ADDRESS="; curl "http://169.254.169.254/latest/meta-data/public-ipv4"; } >> .env
 
+# install dependencies
 npm install 
 
+# start the application
 npm run start
