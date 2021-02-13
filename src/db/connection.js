602211ed8c5170648e1665ea4e6c778c0477aafa
Rajat Sharma <rajat901cx@gmail.com>
2021-02-13 14:54:20 +0530

    Docker Mongo URI Fix

--- src/db/connection.js
+++ src/db/connection.js
@@ -4,9 +4,11 @@ file to setup connection with the Mongo DB database by using Mongoose ODM
 
 const mongoose = require('mongoose')
 
-const dockerURL = 'mongodb://mongo:27017/XMeme-api'
+const mongoURL = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://localhost:27017/XMeme-api'
 
-mongoose.connect('mongodb://localhost:27017/XMeme-api' || dockerURL, {
+console.log(mongoURL)
+
+mongoose.connect(mongoURL, {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
     useCreateIndex: true
